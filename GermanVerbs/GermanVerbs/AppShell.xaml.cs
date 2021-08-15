f3b2f21a8d7a65f793418c68e8e143c184b6ffa6
SergeyKodolov <kodolov-s@mail.ru>
2021-08-15 18:57:52 +0300

    Search with suggestions in the database added

---
+++ GermanVerbs/GermanVerbs/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GermanVerbs
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }        
+    }
+}
